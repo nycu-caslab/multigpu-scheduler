d9a6b2e3c01bcd9853213ddcef2bbbd2732343d9
Muller Lee <s094392@gmail.com>
2023-04-18 22:45:13 +0000

    Profile

---
+++ evaluate.py
@@ -0,0 +1,16 @@
+def main():
+    s = list()
+    e = list()
+    while True:
+        try:
+            start, end = tuple(map(int, input().split()))
+            s.append(start)
+            e.append(end)
+            s.sort()
+            e.sort()
+        except EOFError:
+            print(f"Start: {s[0]}, End: {e[-1]}, Latency: {e[-1] - s[0]}")
+            break
+
+if __name__ == "__main__":
+    main()
